1e48abf83a57206e722ddd142042fdccca55da80
Robin Laugs <Laugslander@users.noreply.github.com>
2016-11-29 14:29:19 +0100

    Implementated RMI and created a starting point for the application.

---
+++ Soccar [Game-Server]/src/nl/soccar/gameserver/ui/Main.java
@@ -0,0 +1,102 @@
+package nl.soccar.gameserver.ui;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import nl.soccar.gameserver.rmi.GameServerController;
+
+/**
+ * Entry point of the Game server application for te Soccar game.
+ *
+ * @author PTS34A
+ */
+public class Main implements Runnable{
+
+    private static final Logger LOGGER = Logger.getLogger(Main.class.getSimpleName());
+
+    private static final String COMMAND_EXIT = "exit";
+
+    private final GameServerController controller;
+    private static Thread t;
+
+    /**
+     * Constructor used for initiation of a Main object. The logger is being
+     * configured, the welcome message is printed and the MainServerController
+     * is instantiated.
+     */
+    public Main() {
+        printWelcomeMessage();
+        controller = new GameServerController();
+    }
+
+    /**
+     * Start the application and starts a new thread that listens for keyboard
+     * input for closing the application.
+     *
+     * @param args Commandline arguments that are not used.
+     */
+    public static void main(String[] args) {
+        t = new Thread(new Main());
+        t.start();
+    }
+
+    // Thread that scans keyboard input for exit keyword to quit the application.
+    @Override
+    public void run() {
+        try (Scanner scanner = new Scanner(System.in)) {
+            printDevider();
+            while (true) {
+                String input = scanner.nextLine();
+                if (processInput(input)) {
+                    break;
+                }
+            }
+        }
+    }
+
+    /**
+     * Processes the input that the user types in the command line.
+     *
+     * @param input The input that the user types in the command line.
+     * @return True when the exit command is recognized.
+     */
+    private boolean processInput(String input) {
+        if (input.equalsIgnoreCase(COMMAND_EXIT)) {
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * Prints the welcome message of the main server that includes the
+     * IP-address of the host machine.
+     */
+    private static void printWelcomeMessage() {
+        printDevider();
+        System.out.println("MAIN SERVER STARTED");
+        printIpAddress();
+        System.out.println("TYPE 'EXIT' TO STOP THE SERVER");
+        printDevider();
+    }
+
+    /**
+     * Prints the IP-address of the host machine.
+     */
+    private static void printIpAddress() {
+        try {
+            System.out.printf("IP ADDRESS: %s %n", InetAddress.getLocalHost().getHostAddress());
+        } catch (UnknownHostException e) {
+            LOGGER.log(Level.SEVERE, "An error occurred while printing the IP address.", e);
+        }
+    }
+
+    /**
+     * Prints a deviding line of dashes.
+     */
+    private static void printDevider() {
+        System.out.println("--------------------------------------------------------------------------------------------------------------------------------");
+    }
+
+}
