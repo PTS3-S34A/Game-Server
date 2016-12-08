9da8c6fd8ba9c0a0bb5747bc4502bc82dc5bc08e
Limpens,Raymond R.H.B. <Ranray@users.noreply.github.com>
2016-12-08 12:27:52 +0100

    Added requested changes.

--- Soccar [Game-Server]/src/nl/soccar/gameserver/message/handler/StartGameMessageHandler.java
+++ Soccar [Game-Server]/src/nl/soccar/gameserver/message/handler/StartGameMessageHandler.java
@@ -16,28 +16,29 @@ public final class StartGameMessageHandler extends MessageHandler<StartGameMessa
 
     @Override
     protected void handle(Connection connection, StartGameMessage message) throws Exception {
-       Player player = connection.getPlayer();
-       if (player == null) {
-           connection.close();
-           return;
-       }
-       
-       Session session = player.getCurrentSession();
-       if (session == null) {
-           connection.close();
-           return;
-       }
+        Player player = connection.getPlayer();
+        if (player == null) {
+            connection.close();
+            return;
+        }
+
+        Session session = player.getCurrentSession();
+        if (session == null) {
+            connection.close();
+            return;
+        }
+        
         SessionController.getInstance().startGame(session);
     }
 
     @Override
     protected void encode(Connection connection, StartGameMessage message, ByteBuf buf) throws Exception {
-        throw new UnsupportedOperationException("Encoding is not supported on Server."); 
+        throw new UnsupportedOperationException("Encoding is not supported on Server.");
     }
 
     @Override
     protected StartGameMessage decode(Connection connection, ByteBuf buf) throws Exception {
         return new StartGameMessage();
     }
-    
+
 }
