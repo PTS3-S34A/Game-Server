6dcfa39d7293b0cead27eb4b46907a2cb05e5fa4
Lesley Vente <BeeHiveJava@users.noreply.github.com>
2016-12-20 12:43:43 +0100

    Update message id.

--- Soccar [Game-Server]/src/nl/soccar/gameserver/controller/socnet/message/LeaveSessionMessage.java
+++ Soccar [Game-Server]/src/nl/soccar/gameserver/controller/socnet/message/LeaveSessionMessage.java
@@ -8,12 +8,12 @@ import nl.soccar.socnet.message.MessageEvent;
 /**
  * @author PTS34A
  */
-@MessageEvent(id = MessageConstants.PLAYER_LEAVE_SESSION_MESSAGE_ID, handler = LeaveSessionMessageHandler.class)
+@MessageEvent(id = MessageConstants.LEAVE_SESSION_MESSAGE_ID, handler = LeaveSessionMessageHandler.class)
 public class LeaveSessionMessage extends Message {
 
     @Override
     public int getId() {
-        return MessageConstants.PLAYER_LEAVE_SESSION_MESSAGE_ID;
+        return MessageConstants.LEAVE_SESSION_MESSAGE_ID;
     }
 
 }
