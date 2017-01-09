8f2113c8c911b577709fdc182dd4b31f7462dc99
Limpens,Raymond R.H.B. <Ranray@users.noreply.github.com>
2017-01-09 11:44:01 +0100

    Refactor PlayerWrapper clas.(Added JavaDoc)

--- Soccar [Game-Server]/src/nl/soccar/gameserver/model/PlayerWrapper.java
+++ Soccar [Game-Server]/src/nl/soccar/gameserver/model/PlayerWrapper.java
@@ -8,6 +8,8 @@ import nl.soccar.library.enumeration.Privilege;
 import nl.soccar.socnet.connection.Connection;
 
 /**
+ * Wrapper for the player class.
+ * 
  * @author PTS34A
  */
 public final class PlayerWrapper {
@@ -15,11 +17,22 @@ public final class PlayerWrapper {
     private final Connection connection;
     private final Player player;
 
+    /**
+     * Initializes the PlayerWrapper class.
+     * 
+     * @param connection The connection of the player, not null.
+     * @param player The player, not null.
+     */
     public PlayerWrapper(Connection connection, Player player) {
         this.connection = connection;
         this.player = player;
     }
 
+    /**
+     * Gets the current session of the player.
+     * 
+     * @return SessionWrapper The current session of the player.
+     */
     public SessionWrapper getCurrentSession() {
         Session session = player.getCurrentSession();
         if (session == null) {
@@ -29,26 +42,56 @@ public final class PlayerWrapper {
         return GameServer.getInstance().getSessionController().getSession(session.getRoom().getName());
     }
 
+    /**
+     * Gets the id of the player.
+     * 
+     * @return int The id of the player.
+     */
     public int getPlayerId() {
         return player.getPlayerId();
     }
 
+    /**
+     * Gets the username of the player.
+     * 
+     * @return String The username of the player.
+     */
     public String getUsername() {
         return player.getUsername();
     }
 
+    /**
+     * Gets the type of the car.
+     * 
+     * @return CarType The type of the car.
+     */
     public CarType getCarType() {
         return player.getCarType();
     }
 
+    /**
+     * Gets the privilege of the player.
+     * 
+     * @return Privilege The privilege of the player.
+     */
     public Privilege getPrivilege() {
         return player.getPrivilege();
     }
 
+    /**
+     * Gets the connection of the player.
+     * 
+     * @return Connection The connection of the player.
+     */
     public Connection getConnection() {
         return connection;
     }
 
+    /**
+     * Unwraps the player object.
+     * 
+     * @return Player The player.
+     */
     public Player unwrap() {
         return player;
     }
