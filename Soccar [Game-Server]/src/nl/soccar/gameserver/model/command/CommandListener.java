b63e7c5633161016cad47f0429d026e6c12045d8
Limpens,Raymond R.H.B. <Ranray@users.noreply.github.com>
2017-01-09 11:44:29 +0100

    Refactor CommandListener clas.(Added JavaDoc)

--- Soccar [Game-Server]/src/nl/soccar/gameserver/model/command/CommandListener.java
+++ Soccar [Game-Server]/src/nl/soccar/gameserver/model/command/CommandListener.java
@@ -7,6 +7,8 @@ import nl.soccar.gameserver.model.session.SessionWrapper;
 import nl.soccar.library.enumeration.Privilege;
 
 /**
+ * Listener for listening to Commands.
+ *
  * @author PTS34A
  */
 public abstract class CommandListener {
@@ -14,17 +16,44 @@ public abstract class CommandListener {
     private final Privilege minimumPrivilege;
     private final boolean hostOnly;
 
+    /**
+     * Initializes the CommandListener class.
+     *
+     * @param minimumPrivilege The minimum privilege to execute commands, not
+     * null.
+     */
     public CommandListener(Privilege minimumPrivilege) {
         this(minimumPrivilege, false);
     }
 
+    /**
+     * Intializes the CommandListener class.
+     *
+     * @param minimumPrivilege The minimum privilege to execute commands, not
+     * null.
+     * @param hostOnly True if the command may only be executed on the host
+     * itself, false if the commands need to be executed on the whole Room.
+     */
     public CommandListener(Privilege minimumPrivilege, boolean hostOnly) {
         this.minimumPrivilege = minimumPrivilege;
         this.hostOnly = hostOnly;
     }
 
+    /**
+     * Executes the command in the Chat.
+     *
+     * @param player The player that is executing the command, not null.
+     * @param room The Room where the command needs to be executed in, not null.
+     * @param command The command that needs to be executed, not null.
+     */
     public abstract void execute(PlayerWrapper player, RoomWrapper room, Command command);
 
+    /**
+     * Executes the command in the Chat with the set Privileges.
+     * 
+     * @param player The player that is executing the command, not null.
+     * @param command The command that needs to be executed, not null.
+     */
     public final void executePrivileged(PlayerWrapper player, Command command) {
         SessionWrapper session = player.getCurrentSession();
         if (session == null) {
