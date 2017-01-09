84ed8369e47cb6f9022b8e0219747b589638f268
Limpens,Raymond R.H.B. <Ranray@users.noreply.github.com>
2017-01-09 12:06:50 +0100

    Refactor JoinSessionMessage class. (Added JavaDoc)

--- Soccar [Game-Server]/src/nl/soccar/gameserver/controller/socnet/message/JoinSessionMessage.java
+++ Soccar [Game-Server]/src/nl/soccar/gameserver/controller/socnet/message/JoinSessionMessage.java
@@ -33,6 +33,12 @@ public final class JoinSessionMessage extends Message {
         settings = null;
     }
 
+    /**
+     * Intializes the JoinSessionMessage class.
+     * 
+     * @param roomName The given room name, not null.
+     * @param password The given password, not null.
+     */
     public JoinSessionMessage(String roomName, String password) {
         this.roomName = roomName;
         this.password = password;
@@ -42,6 +48,14 @@ public final class JoinSessionMessage extends Message {
         settings = null;
     }
 
+    /**
+     * Intializes the JoinSessionMessage class.
+     * 
+     * @param status The status of joining a Session, not null.
+     * @param roomName The roomname, not null.
+     * @param capacity The capcity of the room, not null.
+     * @param settings The game settings, not null.
+     */
     public JoinSessionMessage(Status status, String roomName, int capacity, GameSettings settings) {
         this.status = status;
         this.roomName = roomName;
@@ -50,22 +64,47 @@ public final class JoinSessionMessage extends Message {
         this.settings = settings;
     }
 
+    /**
+     * Gets the Room name.
+     * 
+     * @return String The room name.
+     */
     public String getRoomName() {
         return roomName;
     }
 
+    /**
+     * Gets the password of the Room.
+     * 
+     * @return String The password of the Room.
+     */
     public String getPassword() {
         return password;
     }
 
+    /**
+     * Gets the status of the Joiningsession message.
+     * 
+     * @return Status The stauts of the JoiningSession message.
+     */
     public Status getStatus() {
         return status;
     }
 
+    /**
+     * Gets the capacity of the Room.
+     * 
+     * @return int The capcity of the Room.
+     */
     public int getCapacity() {
         return capacity;
     }
 
+    /**
+     * Gets the gamesettings of the game.
+     * 
+     * @return The gamesettings of the game.
+     */
     public GameSettings getGameSettings() {
         return settings;
     }
@@ -75,6 +114,9 @@ public final class JoinSessionMessage extends Message {
         return MessageConstants.JOIN_SESSION_MESSAGE_ID;
     }
 
+    /**
+     * Enum for the satus of joining a session.
+     */
     public enum Status {
         SUCCESS,
         SESSION_NON_EXISTENT,
