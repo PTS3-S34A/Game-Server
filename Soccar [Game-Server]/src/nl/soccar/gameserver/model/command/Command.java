012e19ebaf26e7e00963524cf54f66ccec97ff68
Limpens,Raymond R.H.B. <Ranray@users.noreply.github.com>
2017-01-09 11:44:12 +0100

    Refactor Command clas.(Added JavaDoc)

--- Soccar [Game-Server]/src/nl/soccar/gameserver/model/command/Command.java
+++ Soccar [Game-Server]/src/nl/soccar/gameserver/model/command/Command.java
@@ -1,6 +1,8 @@
 package nl.soccar.gameserver.model.command;
 
 /**
+ * A Command for the Chat in the room.
+ * 
  * @author PTS34A
  */
 public final class Command {
@@ -8,15 +10,31 @@ public final class Command {
     private final String name;
     private final String[] arguments;
 
+    /**
+     * Intializes the Command class.
+     * 
+     * @param name The Command name, not null.
+     * @param arguments The arguments for the command, not null.
+     */
     public Command(String name, String[] arguments) {
         this.name = name;
         this.arguments = arguments;
     }
 
+    /**
+     * Gets the command name.
+     * 
+     * @return String The command name.
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * Gets the arguments of the command.
+     * 
+     * @return String[] The arguments of the command.
+     */
     public String[] getArguments() {
         return arguments;
     }
